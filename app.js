dedf3bb399b144e2a593af25f55dce68db38f2a1
Bekki Young <moonbladez@hotmail.com>
2020-04-24 21:22:27 +0200

    basic structure

---
+++ app.js
@@ -0,0 +1,6 @@
+var inputs = document.querySelectorAll(".controls input");
+function handleUpdate() {
+    var suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty("--" + this.name, this.value + suffix);
+}
+inputs.forEach(function (input) { return input.addEventListener("change", handleUpdate); });
